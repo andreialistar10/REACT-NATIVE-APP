d805a412e59d3e82306a58be048bad06d53f0ba5
andreialistar10 <andrei.alistar98@gmail.com>
2019-12-28 16:07:37 +0200

    Show map on screen

--- products/MyMap.js
+++ products/MyMap.js
@@ -1,7 +1,8 @@
 import { getLogger } from "../core";
 import React from "react";
-import { Text, View } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import { ProductContext } from './ProductContext';
+import MapView from 'react-native-maps';
 
 const log = getLogger('MyMap');
 
@@ -10,10 +11,42 @@ export const MyMap = () => {
     return (
         <ProductContext.Consumer>
             {() => (
-                <View>
-                    <Text>MyMap</Text>
+                <View style ={styles.container}>
+                    <MapView
+                        style={styles.map}
+                        initialRegion = {{
+                            latitude: 45,
+                            longitude: 26,
+                            latitudeDelta: 0.0922,
+                            longitudeDelta: 0.0421,
+                        }}
+                        showsUserLocation={true}
+                    />
                 </View>
             )}
         </ProductContext.Consumer>
     )
 };
+
+MyMap.navigationOptions = () => ({
+    headerTitle:"My Location"
+});
+
+const styles = StyleSheet.create({
+    container: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+    },
+    map: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+    },
+});
